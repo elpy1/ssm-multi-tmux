e6ed7ab56edffffbcc569d220d8a2d064ead8d2c
elpy1 <elpy@opsdevops.dev>
2021-06-06 03:33:02 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+
+# ssm-multi-tmux  
+Run an interactive AWS SSM command in synchronised tmux panes on one or more EC2 instances by specifying the autoscaling group name or tag key and value.  
+  
+## Getting Started  
+These instructions will help you setup the script on your local machine. See usage for further information.
+  
+### Prerequisites  
+The following must be installed:
+ - `tmux` (https://github.com/tmux/tmux)
+ - `aws`  (https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install)
+ - `session-manager-plugin` (https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html#install-plugin-linux)
+
+  
+### Installing  
+Clone this repo or download `multissm.sh` script
+```
+git clone git@github.com:elpy1/ssm-multi-tmux.git
+```  
+  
+Copy the script to a local `bin` directory  
+```  
+cp ssm-multi-tmux/multissm.sh ~/.local/bin  
+```
+  
+Ensure directory is in your `PATH`
+```  
+echo "PATH="{PATH:+${PATH}:}$HOME/.local/bin" >> ~/.bashrc
+```
+  
+## Usage  
+The script can be used by specifying either an EC2 autoscaling group name or the tag key and tag value of your target instances, as well as the command to run on the remote instances. Ensure AWS environment variables are available via your tool of choice e.g. `AWS_PROFILE` or `AWS_ACCESS_KEY_ID`, `AWS_SECRET_ACCESS_KEY` and `AWS_SESSION_TOKEN`
+```
+Usage: multissm.sh [-h] [-t <key>] -v <value> | -a <asg name> [--] <command>  
+Try 'multissm.sh --help' for more information.
+```
+
+### Connect to EC2 instances in an autoscaling group
+`htop` on instances in autoscaling group **wordpress-prod**:
+  
+```  
+multissm.sh --asg wordpress-prod htop
+```
+  
+tail `httpd` journal log output on instances in autoscaling group **drupal-web-asg**:
+```
+multissm.sh --asg drupal-web-asg -- journalctl -f -u httpd
+```
+  
+### Connect to EC2 instances with specific tag
+Execute `bash` on instances with a tag key of **Name** and tag value of **prod-internal-cms02**:
+```
+multissm.sh --tag Name --value prod-internal-cms02 -- bash 
+```
+   
+output `date` every second  on instances with tag key **app-server** and tag value **drupal-frontend**:
+```
+multissm.sh --tag app-server --value drupal-frontend -- watch -t -n1 date +%s 
+```
+## Demo  
+![multissm demo htop](demo/multissm.gif)
+  
+## Authors  
+* **elpy1** - https://github.com/elpy1  
+  
+## License  
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ multissm.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+#set -x
+
+declare -r BASENAME=${0##*/}
+
+main () {
+  local ids k v args=()
+
+  while (( $# )); do
+    case $1 in
+      -t|--tag)
+        (( $# >= 5 )) || die "$(usage)"
+        shift; k="$1"
+        ;;
+      -a|--asg|-v|--value)
+        args+=( "${k:-aws:autoscaling:groupName}" )
+        shift; args+=( "$1" )
+        ;;
+      -h|--help) help; exit ;;
+      --)        shift; break ;;
+      -*)        die "$(usage)" ;;
+      *)         args+=( "$1" ) ;;
+    esac
+    shift
+  done
+
+  set -- ${args[@]:+"${args[@]}"} "$@"
+
+  (( $# >= 3 )) || die "$(usage)"
+
+  for x in aws tmux session-manager-plugin; do
+    has "$x"
+  done
+
+  ids="$(get_instances "$1" "$2")"
+
+  tmux_ssm_session "${ids}" "sudo ${*:3}"
+}
+
+tmux_ssm_session () {
+  local session instances="$1" ssm_cmd="$2"
+
+  session="${BASENAME%%.*}-$(mktemp -u XXXXXX)"
+
+  tmux new-session -d -s "${session}" >/dev/null
+
+  trap 'tmux kill-session -t "${session}" 2>/dev/null' EXIT
+
+  for i in ${instances}; do
+    tmux select-layout -t "${session}:0" tiled
+    tmux split-window -t "${session}:0" \
+      "aws ssm start-session \
+      --document-name AWS-StartInteractiveCommand \
+      --parameters command='[\"${ssm_cmd}\"]' \
+      --target ${i}"
+  done
+
+  tmux kill-pane -t "${session}:0.0"
+  tmux select-layout -t "${session}:0" tiled
+  tmux select-window -t "${session}:0"
+  tmux set-window-option synchronize-panes on 2>/dev/null
+  exec tmux attach-session -t "${session}:0"
+}
+
+get_instances () {
+  local instances k="$1" v="$2"
+
+  instances="$(aws ec2 describe-instances \
+    --query 'Reservations[*].Instances[*].InstanceId' \
+    --filters Name=tag:"${k}",Values="${v}" \
+    Name=instance-state-code,Values=16 \
+    --output text)" || die "[${BASENAME}] error: aws: returned status $?"
+
+  if [[ -z ${instances} ]]; then
+    die "[${BASENAME}] error: aws: returned no EC2 instances"
+  fi
+
+  printf '%s' "${instances}"
+}
+
+
+help () {
+  cat <<EOF
+Usage: ${BASENAME} [--tag KEY] --value VALUE | --asg NAME [--] COMMAND [ARG ...]
+Run an interactive AWS SSM command in synchronised tmux panes on one or more EC2
+instances by specifying the autoscaling group name or tag key and value.
+
+ Optional parameters:
+  -t, --tag KEY              EC2 instance tag key
+
+ Required parameter:
+  -a, --asg NAME             EC2 autoscaling group name; or
+  -v, --value VALUE          EC2 instance tag value (if --tag provided)
+
+ Help options:
+  -h, --help                 Provide this help output
+
+This script uses EC2 describe-instances to query for the autoscaling group or
+tag key and value provided. If successful a local tmux session is created.
+COMMAND is run on each returned instance in synchronised tmux panes using AWS
+Session Manager plugin. Use -- if COMMAND requires its own arguments.
+
+Examples:
+
+  ${BASENAME} --tag Name --value prod-internal-cms02 -- bash
+  ${BASENAME} --asg wordpress-prod htop
+  ${BASENAME} --asg drupal-web-asg -- journalctl -f -u httpd
+  ${BASENAME} --tag webgroup --value drupal-frontend -- watch -t -n1 date +%s
+EOF
+}
+
+usage () {
+  cat <<EOF
+Usage: ${BASENAME} [-h] [-t <key>] -v <value> | -a <asg name> [--] <command>
+Try '${BASENAME} --help' for more information.
+EOF
+}
+
+has () {
+  command -vp "$1" &>/dev/null || die "error: executable '${x}' not found"
+}
+
+die () {
+  printf '%s\n' "$*" >&2; exit 1
+}
+
+main "$@"
